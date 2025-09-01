1e4aee55aec8b9e0c9ba6db7572a07dd4dcd29b4
Ashish Kumar <theashishmavi@outlook.com>
2025-09-01 15:54:41 +0530

    UI added & layer 1 work completed

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from flask import render_template
 
 app = create_app()
+
+@app.route('/')
+def dashboard():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
